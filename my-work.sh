7b9cc7acaf8a03b3960ef61d4dbdfbd3a4c3327f
Piotr Karkut <karkucik@gmail.com>
2018-02-05 15:40:56 +0100

    Add my-work.sh script

---
+++ my-work.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+defdate=`date "+%Y-%m"`
+
+read -p "Data logu rrrr-mm [${defdate}]: " date </dev/tty
+
+if [ -z "$date" ]; then
+  date=${defdate}
+fi
+
+
+defgituser=`git config user.email`
+read -p "Email użytkownika git: [${defgituser}]: " gituser </dev/tty
+
+if [ -z "$gituser" ]; then
+  gituser=${defgituser}
+fi
+echo
+echo
+
+echo "Zmiany w ${PWD##*/} na miesiąc ${date} dla ${gituser}"
+echo
+
+git log --all --name-only --pretty=format: --after=${date}-01 --before=${date}-31 --author=${gituser} | grep . | sort | uniq -c | sort -r | awk '{print $2,$1}' | column -t -o " | " -N Plik,"Ilość zmian"
