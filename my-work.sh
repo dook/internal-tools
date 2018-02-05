b3ff2aa6593bbf7ecbdef80a0136dc033fc61f82
Piotr Karkut <karkucik@gmail.com>
2018-02-05 15:45:17 +0100

    Add spearators

--- my-work.sh
+++ my-work.sh
@@ -17,7 +17,10 @@ fi
 echo
 echo
 
+echo "========================================================================="
 echo "Zmiany w ${PWD##*/} na miesiąc ${date} dla ${gituser}"
 echo
 
 git log --all --name-only --pretty=format: --after=${date}-01 --before=${date}-31 --author=${gituser} | grep . | sort | uniq -c | sort -r | awk '{print $2,$1}' | column -t -o " | " -N Plik,"Ilość zmian"
+
+echo "========================================================================="
