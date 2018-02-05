69c81bcb1f4fb868cb854af1a5f09dd7d23035e1
Piotr Karkut <karkucik@gmail.com>
2018-02-05 17:01:24 +0100

    Change commits to lines

--- my-work.sh
+++ my-work.sh
@@ -21,6 +21,31 @@ echo "========================================================================="
 echo "Zmiany w ${PWD##*/} na miesiąc ${date} dla ${gituser}"
 echo
 
-git log --all --name-only --pretty=format: --after=${date}-01 --before=${date}-31 --author=${gituser} | grep . | sort | uniq -c | sort -r | awk '{print $2,"->",$1}'
+
+pyscr="$(cat <<-EOF
+import subprocess
+from collections import Counter
+
+data = Counter()
+
+log = subprocess.check_output('git log --all --after=${date}-01 --before=${date}-31 --author=${gituser} --pretty=format:"%h"', shell=True)
+
+for commit in log.splitlines():
+  commit = commit.decode()
+  try:
+    stats = subprocess.check_output('git diff %s^ %s --numstat' % (commit, commit), shell=True, stderr=open('/dev/null'))
+  except Exception:
+    stats = subprocess.check_output('git diff 4b825dc642cb6eb9a060e54bf8d69288fbee4904 %s --numstat' % commit, shell=True)
+  for stat in stats.splitlines():
+    # print(stat.decode())
+    ins, dels, f = stat.split(maxsplit=2)
+    data[f.decode()] += (int(ins) + int(dels))
+
+for f, ch in data.items():
+  print("Plik %s, zmienionych linii: %s" % (f, ch))
+EOF
+)"
+
+python > /dev/tty <<< ${pyscr}
 
 echo "========================================================================="
